1601a0bae1ca8cd133fa557b0491601edf66b25e
김현기 <11609005+rubi1541@users.noreply.github.com>
2020-04-08 23:44:16 +0900

    check socketIo room system

--- chatServer/main.js
+++ chatServer/main.js
@@ -4,19 +4,33 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var redis = require('socket.io-redis');
 
-
-io.adapter(redis({host:'redis',port:6379}));
-
+io.adapter(redis({ host: 'redis', port: 6379 }));
 
 server.listen(80, function () {
     console.log('Socket IO server listening on port 80');
 });
 
-
 io.on('connection', function (socket) {
-    console.log("conneted")
-    socket.on('message', function(data){
-        console.log('MessageArrive',data)
-        //socket.broadcast.emit('message', data);
+    console.log("conneted");
+
+    socket.on('join', function (data) {
+        socket.join('room' + data);
     });
+
+    socket.on('message', function (data) {
+        //socket.in().emit(data.text);
+        //console.log('MessageArrive', data);
+    });
+
+    socket.on('check', function (data) {
+        io.of('/').adapter.allRooms((err, rooms) => {
+            console.log(rooms); // an array containing all rooms (accross every node)
+        });
+    })
+
+    socket.on('check2', function (data) {
+        io.of('/').adapter.clients((err, clients) => {
+            console.log(clients); // an array containing all connected socket ids
+        });
+    })
 });
